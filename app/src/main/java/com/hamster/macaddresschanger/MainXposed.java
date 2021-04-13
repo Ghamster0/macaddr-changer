c50a3eb907b489b91f80be0c7005359df9e3cd4b
guangwei <guangwei@sogou-inc.com>
2021-04-13 20:56:34 +0800

    init

---
+++ app/src/main/java/com/hamster/macaddresschanger/MainXposed.java
@@ -0,0 +1,14 @@
+package com.hamster.macaddresschanger;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainXposed implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app:" + lpparam.packageName);
+    }
+
+}
